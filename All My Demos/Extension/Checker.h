f18be6976898bf63f0531ae2fca2ac1b14b49721
kwokpingl <kwokpinglau.119@gmail.com>
2017-12-09 21:56:57 +0800

    no message

---
+++ All My Demos/Extension/Checker.h
@@ -0,0 +1,14 @@
+//
+//  Checker.h
+//  All My Demos
+//
+//  Created by Jimmy on 2017/12/8.
+//  Copyright © 2017年 Jimmy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <mach/mach.h>
+
+@interface Checker : NSObject
++ (BOOL) is64bitHardware;
+@end
---
+++ All My Demos/M/HardwareChecker.swift
@@ -0,0 +1,14 @@
+//
+//  HardwareChecker.swift
+//  All My Demos
+//
+//  Created by Jimmy on 2017/12/8.
+//  Copyright © 2017年 Jimmy. All rights reserved.
+//
+
+import Foundation
+import MachO
+
+struct Tester {
+    
+}
